1dda7d3ee683bf6d008b227292c6855d2f13ad23
mengchenwang <mengchenwang@hotmail.com>
2017-12-14 23:20:55 +0000

    switch

--- src/com/mengchen/Main.java
+++ src/com/mengchen/Main.java
@@ -3,6 +3,19 @@ package com.mengchen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int switchValue = 1;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("was not 1 or 2");
+                break;
+        }
+
     }
 }
